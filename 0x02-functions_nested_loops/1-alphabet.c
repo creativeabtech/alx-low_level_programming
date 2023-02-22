e70fee1705aab4a4324ff27f419e7f305886636b
creativeabtech <122811945+creativeabtech@users.noreply.github.com>
2023-02-22 16:24:30 +0100

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - is to print alphabet
+ *
+ * Return: 0;
+ *
+ */
+void print_alphabet(void)
+	{
+		char myLetter = 'a';
+
+		while (myLetter <= 'z')
+		{
+			_putchar(myLetter);
+			myLetter++;
+
+		}
+		_putchar('\n');
+}
