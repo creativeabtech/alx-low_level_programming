f8d6380f81822af64089bda09949ab9e81097781
creativeabtech <uchebutone@gmail.com>
2023-02-22 23:46:32 -0800

    Print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,25 @@
 #include "main.h"
+/**
+ *print_alphabet_x10-print alphabet
+ *
+ *Return: 0
+ *
+ */
 
+void print_aphabet_10(void)
+{
+  int start = 0;
+  char letter = 'a';
+  
+  while (start <=10)
+    {
+      letter = 'a';
+      while (letter <= 'z')
+	{
+	  _putchar(letter);
+	  letter++;
+	}
+      _putchar('\n');
+      start++;
+    }
+}
