4f82611d937acacb4325a73f286e539a691f9be1
creativeabtech <122811945+creativeabtech@users.noreply.github.com>
2023-02-27 21:25:59 +0100

    Reset to 98 fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- *reset_to_98 - rest the value of n to 98
- *
- *@n: use 98
- *Return: 0 alwasy
+ * reset_to_98 - reset value of pointer
+ * @n: argument
+ * Return: void
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }
